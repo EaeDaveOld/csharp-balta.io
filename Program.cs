d940cf1e09ca40e54b45dccd1031dcce13cec621
EaeDave <daveisrd@gmail.com>
2024-03-30 14:35:14 -0300

    Sintaxe básica do C#

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;  // Importando uma biblioteca, já não se faz mais necessário importar o System, pois o .NET já faz isso por padrão.
+namespace MeuApp  // Declarando o "package".
+{
+    class Program  // Declarando a classe.
+    {
+        static void Main(string[] args)  // Método principal, onde é executado o programa por padrão.
+        {
+            var texto = "Testando";
+            Console.WriteLine(texto);
+        }
+    }
+}
