55bf54b9c41ba5be3d1f635ec05a471b20bdbb4e
EaeDave <daveisrd@gmail.com>
2024-03-30 15:39:37 -0300

    Declarando variáveis

--- Program.cs
+++ Program.cs
@@ -5,7 +5,14 @@ namespace MeuApp  // Declarando o "package" - Divisão lógica.
     {
         static void Main(string[] args)  // Método principal, onde é executado o programa por padrão.
         {
+            int idade;  // Correto, inicia com zero.
+            int anos = 22;  // Correto, inicia com 22.
+            var numero = 22;  // Correto, inicia com 22.
+            // var numero2;  // Errado.
             var texto = "Testando";
+            // Console.WriteLine(idade);
+            Console.WriteLine(anos);
+            Console.WriteLine(numero);
             Console.WriteLine(texto);
         }
     }
--- Teste/Program.cs
+++ Teste/Program.cs
@@ -1,7 +1,8 @@
 namespace MeuApp
 {
-    class Program  // Mesmo sendo um outro arquivo, em um outro diretório, o C# já identificou que o namespace "MeuApp" já possui uma declaração de classe "Program"
+    /* class Program  // Mesmo sendo um outro arquivo, em um outro diretório, o C# já identificou que o namespace "MeuApp" já possui uma declaração de classe "Program"
     {
 
     }
+    */
 }
